0fe0f6d731be00db914a4c61b23eb90d2075a911
Esteban Muñoz <sergio.barragan@quick.com.co>
2022-01-03 14:43:15 -0500

    Feature(Back) add enviroment variables

--- 01-band_names_server/index.js
+++ 01-band_names_server/index.js
@@ -1,8 +1,13 @@
+const path = require("path");
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const publicPath = path.resolve(__dirname, "public");
 
-app.listen(3000, (e) => {
+app.use(express.static(publicPath));
+
+app.listen(process.env.PORT, (e) => {
   if (e) throw new Error(e);
 
-  console.log("Conectado al puerto ", 3000);
+  console.log("Conectado al puerto ", process.env.PORT);
 });
